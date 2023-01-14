e864f96d5895fe24634d360d79e7ab615227a0a7
Bella Palumbi <christabellapalumbi@gmail.com>
2023-01-14 17:43:30 -0500

    Follow panel v1

---
+++ URPWavePassthroughWorking/Assets/Delay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delay : MonoBehaviour
+{
+
+    public Transform posToFollow;
+    public Transform toLookAt;
+    public float delayAmount;
+    private Vector3 positionVelocity;
+    public float maxSpeed = 10f;
+
+    private Coroutine followCoroutine;
+    
+
+    private IEnumerator FollowPosition() {
+        while(transform.position != posToFollow.position) {
+            transform.position = Vector3.SmoothDamp(transform.position, posToFollow.position, ref positionVelocity, delayAmount);
+            yield return null;
+        }
+
+        followCoroutine = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(toLookAt);
+
+        if(followCoroutine == null) {
+            followCoroutine = StartCoroutine(FollowPosition());
+        }
+
+        //transform.position = cameraTransform.position;
+    }
+}
