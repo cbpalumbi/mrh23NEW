657d340da9921635463f52b0fc258b8798f8f229
Bella Palumbi <christabellapalumbi@gmail.com>
2023-01-14 18:34:22 -0500

    You can reopen the menu with a button

--- URPWavePassthroughWorking/Assets/NewUIManager.cs
+++ URPWavePassthroughWorking/Assets/NewUIManager.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Wave.Native;
 
 public class NewUIManager : MonoBehaviour
 {
     public GameObject startScreen;
     public GameObject instructions;
+    //public GameObject cube;
 
     void Start() {
         startScreen.SetActive(true);
         instructions.SetActive(false);
+    }
+
+    void Update() {
+        
+        if(Interop.WVR_GetInputTouchState(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_A)) {
+            ReShowInstructions();
+        }
+
+        if(Interop.WVR_GetInputTouchState(WVR_DeviceType.WVR_DeviceType_Controller_Left, WVR_InputId.WVR_InputId_Alias1_A)) {
+            ReShowInstructions();
+        }
+
+        if (Input.GetKey(KeyCode.M)) {
+            ReShowInstructions();
+        }
+        
     }
 
     // void Update() {
@@ -26,5 +44,9 @@ public class NewUIManager : MonoBehaviour
 
     public void OnCloseInstructionsButtonClicked() {
         instructions.SetActive(false);
+    }
+
+    void ReShowInstructions() {
+        instructions.SetActive(true);
     }
 }
